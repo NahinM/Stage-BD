2ff3605aa62212e1db9c03d09419d4c7248aa07c
Nahin Munkar <nahinmunkarnm@gmail.com>
2026-03-10 16:30:15 +0600

    added nodemon for dynamic loading

--- server/src/index.js
+++ server/src/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/api/hello', (req, res) => {
+function helloHandler(req, res) {
   res.send({ message: 'Hello, World!' });
-});
+}
+
+app.get('/api/hello', helloHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
