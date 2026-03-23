52ad2b5520c615a72608d8937ffcc16a7676fbef
Nahin Munkar <nahinmunkarnm@gmail.com>
2026-03-23 19:14:45 +0600

    test removed

--- server/src/routes/route.js
+++ server/src/routes/route.js
@@ -1,15 +1,6 @@
 import * as controller from "../controllers/controller.js";
 import { app } from "../config/config.js";
 
-import { getHashedPassword } from "../models/signin/signin.js";
-
 app.get('/api/hello', controller.helloController.getHello);
 app.post('/api/signin', controller.signInController.signIn);
 app.post('/api/signup', controller.signUpController.signUp);
-
-
-app.get('/api/test', async (req,res) => {
-    const hash = await getHashedPassword("NahinM"); 
-    res.json({hashed: hash});
-})
-
