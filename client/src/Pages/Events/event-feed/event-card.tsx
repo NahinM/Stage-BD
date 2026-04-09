34bc9f5d81ba16ecb3ae0e8927f4b57b0edeed27
Nahin Munkar <nahinmunkarnm@gmail.com>
2026-04-09 12:33:39 +0600

    event card disign

---
+++ client/src/Pages/Events/event-feed/event-card.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+export interface EventCardType {
+    id: string,
+    title: string,
+    description: string,
+    type: string,
+    is_free: boolean,
+    category: string,
+}
+
+export function EventCard({Item}: {Item: EventCardType}) {
+    return (
+        <Card className="max-w-md p-0 overflow-visible">
+            <CardHeader className="p-0 relative mb-5">
+                <div className="w-full min-h-50 rounded bg-gray-200"></div>
+                <div className="absolute top-0 left-0 bg-white/80 rounded-md py-1 px-3 transform -translate-y-full font-bold border border-gray-500">
+                    {Item.is_free ? "Free" : "Paid"}
+                </div>
+                <div className="absolute bottom-0 right-0 bg-white/80 rounded-md py-1 px-3 font-bold transform translate-y-1/2">
+                    {Item.type}
+                </div>
+            </CardHeader>
+            <CardContent>
+                <CardTitle className="text-center text-lg">{Item.title}</CardTitle>
+                <CardDescription className="h-25">{Item.description}</CardDescription>
+            </CardContent>
+            <CardFooter>
+                <Button variant="outline" className="w-full">
+                    View Details
+                </Button>
+            </CardFooter>
+        </Card>
+    )
+}
